a7f060ad8b76f5b04f7de599c987849c85d9e953
Ar2Dev <arpego2@gmail.com>
2022-05-01 19:26:20 -0500

    Desarrollo de ejercicios 6

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,28 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+
+  String.prototype.capitalize = function (){
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  }
+  return nombre.capitalize();
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+
+  return cb();
+
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+
+  return cb(n1,n2);
+
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +33,26 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+
+  function sumar (total, num) {
+    return total += num;
+  }
+
+  cb(numeros.reduce(sumar));
+
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+
+  function iterar (item) {
+    return cb(item);
+  }
+
+  array.forEach(iterar);
+
 }
 
 function map(array, cb) {
@@ -35,12 +60,25 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+
+  for ( i = 0; i < array.length; i++  ) {
+    let newarray = array.map(cb);
+    return newarray;
+  }
+
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+
+  function palabras (word) {
+    if (word.charAt(0) === 'a') {
+        return word;
+    }
+  }
+  return array.filter(palabras);
 }
 
 // No modificar nada debajo de esta línea
